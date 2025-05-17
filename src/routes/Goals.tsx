e18b672658bd9f7c33ad5831671bef1e2d2b65d7
Miriam Araújo <miriamaraujoweb@gmail.com>
2025-05-17 20:35:52 +0100

    added request to vercel api get goals

--- src/routes/Goals.tsx
+++ src/routes/Goals.tsx
@@ -1,8 +1,29 @@
+
+import { useEffect, useState } from 'react';
+
 export default function Goals() {
-    
+
+    const [goals, setGoals] = useState([]);
+
+    useEffect(() => {
+        fetch("https://brighsteps-api.vercel.app/api/goals")
+            .then(res => res.json())
+            .then(data => setGoals(data))
+            .catch(err => console.log('Something failed', err));
+    }, [])
+
+
     return (
         <>
             This is the goals page
+
+            {goals.map((goal: { id: number; title: string; completed: boolean }) => (
+                <li key={goal.id}>
+                    <span className={goal.completed ? "line-through text-gray-500" : ''}>
+                        {goal.title}
+                    </span>
+                </li>
+            ))}
         </>
     );
 }
