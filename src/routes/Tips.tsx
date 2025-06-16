6ebd18cddd92bc3e77e3a4bad67519b6aa6e9522
Miriam Araújo <miriamaraujoweb@gmail.com>
2025-06-16 09:38:52 +0100

    added loading spinner to tips page + error

--- src/routes/Tips.tsx
+++ src/routes/Tips.tsx
@@ -5,11 +5,14 @@ import TipRow from '../components/ui/TipRow.tsx';
 import PageTitle from './../components/ui/PageTitle.tsx';
 import axios from 'axios';
 import DataGrid from './../components/ui/DataGrid.tsx';
+import { Spinner } from "flowbite-react";
 
 export default function Tips() {
     const { t, i18n } = useTranslation();
     const lang = i18n.language as 'en' | 'pt';
     const [tips, setTips] = useState<Tip[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string>('');
     const gridHeader = useMemo(() => [
         { id: 1, title: '🎯 ' + t('tips.title') },
         { id: 2, title: '📅 ' + t('tips.description') }
@@ -19,29 +22,41 @@ export default function Tips() {
         axios.get("https://brighsteps-api.vercel.app/api/tips")
             .then(function (response) {
                 setTips(response['data']);
+                setLoading(false);
             })
-            .catch(function (error) {
-                console.log('Oppps, something went wrong.');
-                console.log(error);
+            .catch(function () {
+                setLoading(false);
+                setError(t('common.errorLoadingData'));
             });
-    }, []);
-
+    }, [t]);
 
     return (
         <div className="max-w-5xl mx-auto px-6 py-10 rounded-xl shadow-md bg-[var(--color-container)]">
             <PageTitle title={t('menu.tips')} />
 
-            <DataGrid
-                gridHeader={gridHeader}
-                data={tips}
-                renderRow={(tip) => (
-                    <TipRow
-                        key={tip.id}
-                        tip={tip}
-                        lang={lang}
-                    />
-                )}
-            />
+            {loading ? (
+                <div className="flex justify-center py-10">
+                    <Spinner color="info" aria-label="Loading" />
+                </div>
+            ) : error ? (
+                <div className="flex justify-center py-10">
+                    <p className="text-center text-red-600 font-semibold">
+                        {error}
+                    </p>
+                </div>
+            ) : (
+                        <DataGrid
+                            gridHeader={gridHeader}
+                            data={tips}
+                            renderRow={(tip) => (
+                                <TipRow
+                                    key={tip.id}
+                                    tip={tip}
+                                    lang={lang}
+                                />
+                            )}
+                        />
+            )}
         </div>
     );
 }
